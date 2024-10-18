0e603a6bff497fb54f1909a03107efc8447011d1
9gsingh990 <gsingh1_be21@thapar.edu>
2024-10-18 14:37:15 +0530

    Initial first

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,29 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import Layout from "./layouts/layout"
+import Homepage from "./Pages/Homepage"
+import AuthCallbackPage from "./Pages/AuthCallbackPage"
+import UserProfilePage from "./Pages/UserProfilePage"
+import ProtectedRoute from "./auth/ProtectedRoute"
+import ManageRestaurantPage from "./Pages/ManageRestaurantPage"
+import SearchPage from "./Pages/SearchPage"
+import DetailPage from "./Pages/DetailPage"
+import OrderStatusPage from "./Pages/OrderStatusPage"
+
+function AppRoutes() {
+  return (
+    <Routes>
+        <Route path="/" element={<Layout showHero><Homepage /></Layout>} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/search/:city" element={<Layout showHero={false}><SearchPage/></Layout>} />
+        <Route path="/detail/:restaurantId" element={<Layout showHero={false}><DetailPage/></Layout>} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/order-status" element={<Layout><OrderStatusPage /></Layout>} />
+          <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
+          <Route path="/manage-restaurant" element={<Layout><ManageRestaurantPage /></Layout>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
